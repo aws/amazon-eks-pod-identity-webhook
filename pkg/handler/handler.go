3efdb9ff775f9c8046e393b409f0a1a95f709cbc
Jyoti Mahapatra <jyotima@amazon.com>
2021-08-16 16:01:35 -0700

    add metrics for number of SA annotations being used

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -27,6 +27,7 @@ import (
 
 	"github.com/aws/amazon-eks-pod-identity-webhook/pkg"
 	"github.com/aws/amazon-eks-pod-identity-webhook/pkg/cache"
+	"github.com/prometheus/client_golang/prometheus"
 	"k8s.io/api/admission/v1beta1"
 	admissionregistrationv1beta1 "k8s.io/api/admissionregistration/v1beta1"
 	corev1 "k8s.io/api/core/v1"
@@ -39,6 +40,15 @@ import (
 type podUpdateSettings struct {
 	skipContainers map[string]bool
 	useRegionalSTS bool
+}
+
+var mutations = prometheus.NewCounter(prometheus.CounterOpts{
+	Name: "mutations",
+	Help: "Count of mutations on the pods",
+})
+
+func init() {
+	prometheus.MustRegister(mutations)
 }
 
 // newPodUpdateSettings returns the update settings for a particular pod
@@ -416,6 +426,7 @@ func (m *Modifier) MutatePod(ar *v1beta1.AdmissionReview) *v1beta1.AdmissionResp
 
 	// TODO: klog structured logging can make this better
 	if changed {
+		mutations.Inc()
 		klog.V(3).Infof("Pod was mutated. %s",
 			logContext(pod.Name, pod.GenerateName, pod.Spec.ServiceAccountName, pod.Namespace))
 	} else {
